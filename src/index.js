cd20f959066661b782a39094b6062ec1156cbb92
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-01-21 14:02:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { App2 } from 'components2/App2';
+import { store } from 'components2/redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/react-home-practice07.2">
+      <Provider store={store}>
+        <App2 />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
